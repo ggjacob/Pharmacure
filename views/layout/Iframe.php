f703c8a9774cefc310ff95ac35553418bf761949
Badi <badihaira@gmail.com>
2014-06-10 19:57:20 +0200

    Iframe Layout

---
+++ views/layout/Iframe.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" href="<?=WEBROOT?>public/css/style.css">
+<link rel="stylesheet" href="<?=WEBROOT?>public/css/tab.css">
+<link rel="stylesheet" type="text/css" href="<?=WEBROOT?>public/css/easyui.css">
+<link rel="stylesheet" type="text/css" href="<?=WEBROOT?>public/css/icon.css">
+
+
+<script type="text/javascript" src="<?=WEBROOT?>public/js/jquery-1.4.4.min.js"></script>
+<script type="text/javascript" src="<?=WEBROOT?>public/js/jquery.easyui.min.js"></script>
+<script type="text/javascript" src="<?=WEBROOT?>public/js/app.js"></script>
+
+<?php echo $content_for_layout; ?>
