c916917c8ef3f97d6dbe80f375f082d6eb0b0a95
Voltan D. Black <voltan18@hotmail.com>
2014-07-08 16:47:19 +0200

    Code nettoyé

--- views/Statistiques/index.php
+++ views/Statistiques/index.php
@@ -1,15 +1,13 @@
 
 <div>
-                        <div class="alert alert-icon alert-success">
-                                <i class="fa fa-envelope pull-left"></i>
-                                <div class="text">
-                                    <span class="bold">Succ&egrave;s ! </span> Votre message a &eacute;t&eacute; enregistr&eacute;. Vous serez contact&eacute; tr&egrave;s prochainement...
-                                </div>
-                            </div>
-                            <div class="alert alert-icon alert-danger">
-                                <i class="fa fa-exclamation-circle pull-left"></i>
-                                <div class="text">
-                                    <span class="bold">Erreur ! </span> Veuillez renseigner tout les champs requis...
-                                </div>
-                            </div>
-                    </div>
+	<div class="alert alert-icon alert-success">
+		<div class="text">
+			<span class="bold">Succ&egrave;s ! </span> Votre message a &eacute;t&eacute; enregistr&eacute;. Vous serez contact&eacute; tr&egrave;s prochainement...
+		</div>
+	</div>
+	<div class="alert alert-icon alert-danger">
+		<div class="text">
+			<span class="bold">Erreur ! </span> Veuillez renseigner tout les champs requis...
+		</div>
+	</div>
+</div>
