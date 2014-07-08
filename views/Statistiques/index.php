6e3ffe9962292361a4acfd8e8ca59ce9ebd50f5d
Voltan D. Black <voltan18@hotmail.com>
2014-07-08 16:43:45 +0200

    Ajout des messages de confirmation formulaire
    Ils se trouvent dans l’onglet STATISTIQUES et peuvent être copié/collé
    n’importe ou dans l’application.

--- views/Statistiques/index.php
+++ views/Statistiques/index.php
@@ -1,3 +1,15 @@
-<ul id="featured" class="wrapper clearfix">
-	<h1><?=$view['titre']?></h1>
-</ul>
+
+<div>
+                        <div class="alert alert-icon alert-success">
+                                <i class="fa fa-envelope pull-left"></i>
+                                <div class="text">
+                                    <span class="bold">Succ&egrave;s ! </span> Votre message a &eacute;t&eacute; enregistr&eacute;. Vous serez contact&eacute; tr&egrave;s prochainement...
+                                </div>
+                            </div>
+                            <div class="alert alert-icon alert-danger">
+                                <i class="fa fa-exclamation-circle pull-left"></i>
+                                <div class="text">
+                                    <span class="bold">Erreur ! </span> Veuillez renseigner tout les champs requis...
+                                </div>
+                            </div>
+                    </div>
