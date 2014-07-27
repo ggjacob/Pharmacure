025e2403decf304534475bfdcb31124594fb58b3
badihaidara <badihaidara@gmail.com>
2014-07-27 20:35:39 +0200

    maj panier ventes

--- views/Vente/listeArticles.php
+++ views/Vente/listeArticles.php
@@ -1,6 +1,6 @@
 <table id="sale_table" width="600px" >
 <?php if($view['articles']->count() > 0) : ?>
-	<tr><th>Action</th><th>Article</th><th>prix</th></tr>
+	<tr><th>Action</th><th>Article</th><th>prix</th><th width="35px">Ordonnance</th></tr>
 <?php endif;?>
 </table>
 <div style="height:100px;overflow:auto;">
