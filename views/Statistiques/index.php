ccb24c623faa53adb29511479c2f603f5811b7ca
badihaidara <badihaidara@gmail.com>
2014-10-17 04:46:23 +0200

    devienette
    Qui va savoir ce qu’il y a là bas hehehe

--- views/Statistiques/index.php
+++ views/Statistiques/index.php
@@ -1,4 +1,48 @@
-<script>
+<link rel="stylesheet" type="text/css" href="<?=WEBROOT?>public/css/jquery.gridster.css">
+<link rel="stylesheet" type="text/css" href="<?=WEBROOT?>public/css/dashboard.css">
+        <section class="demo">
+            <div class="gridster">
+                <ul>
+                    <li data-row="1" data-col="1" data-sizex="3" data-sizey="1">
+                        Indicateurs: 
+                        <select name="select">
+                          <option value="value1" selected>Chiffre d'affaire</option> 
+                          <option value="value2">Nombre de Vente</option>
+                          <option value="value3">Marge</option>
+                        </select>
+                    </li>
+                    <li data-row="1" data-col="2" data-sizex="3" data-sizey="1">
+                        De: 
+                        <input type="date" id="debut">
+                        A: 
+                        <input type="date" id="fin">
+                    </li>
+                    <li data-row="2" data-col="1" data-sizex="3" data-sizey="5">
+                        <div id="evol_CA" style="width:330px; height:220px;margin:5px"></div>
+                    </li>
+                    <li data-row="2" data-col="2" data-sizex="3" data-sizey="5">
+                        <div id="evol_stock" style="width:330px; height:220px;margin:5px"></div>
+                    </li>
+                    <li data-row="3" data-col="1" data-sizex="6" data-sizey="1">
+                        <center><h3>Espace pour les filtres</h3></center>
+                    </li>              
+                </ul>
+            </div>
+        </section>
+        <script type="text/javascript" src="<?=WEBROOT?>public/js/jquery.gridster.js" charster="utf-8"></script>
+        
+        <script type="text/javascript">
+            var gridster;
+
+            $(function() {
+                gridtster = $(".gridster > ul").gridster({
+                    widget_margins: [10, 10], 
+                    widget_base_dimensions: [100, 30],
+                    max_cols:4,
+                }).data('gridster');
+            });
+        </script>
+        <script>
 $(function () {
     $('#evol_CA').highcharts({
         chart: {
@@ -42,16 +86,16 @@ $(function () {
             }
         },
         series: [
-        			{
-		            name: '2013',
-		            data: [3.9, 2.5, 3.0, 4.2, 5.0, 8.0, 9.6]
+                    {
+                    name: '2013',
+                    data: [3.9, 2.5, 3.0, 4.2, 5.0, 8.0, 9.6]
 
-			        }, 
-			        {
-		            name: '2014',
-		            data: [4.6, 3.8, 3.4, 5.1, 5.0, 6.7, 11.0]
+                    }, 
+                    {
+                    name: '2014',
+                    data: [4.6, 3.8, 3.4, 5.1, 5.0, 6.7, 11.0]
 
-			        }
+                    }
         ]
     });
 });
@@ -92,41 +136,4 @@ $(function () {
         }]
     });
 });
-
-$(function() {
-    $( "#datepicker" ).datepicker();
-  });
-
 </script>
-<script type="text/javascript" src="<?=WEBROOT?>public/js/jquery-ui.js"></script>
-<link rel="stylesheet" type="text/css" href="<?=WEBROOT?>public/css/jquery-ui.css">
-<table>
-    <tr>
-        <td align="center"> 
-            <br>Type: 
-            <select name="select">
-              <option value="value1" selected>Chiffre d'affaire</option> 
-              <option value="value2">Nombre de Vente</option>
-              <option value="value3">Marge</option>
-            </select>
-        </td>
-        <td align="center">
-            <br>
-            Date: 
-            <input type="text" id="datepicker">
-        </td>
-    </tr>
-	<tr>
-		<td>
-			<tr>
-				<td style="">
-					<div id="evol_CA" style="width:450px; height:300px;margin:10px;"></div>
-				</td>
-				<td>
-					<div id="evol_stock" style="width:450px; height:300px;margin:10px;"></div>
-				</td>
-			</tr>
-		</td>
-	</tr>
-</table>
-
