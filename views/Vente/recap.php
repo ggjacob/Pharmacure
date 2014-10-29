3987d6d76b929db2af959ce8268051102c220683
badihaidara <badihaidara@gmail.com>
2014-10-29 21:47:20 +0100

    correction bug commit speedy

--- views/Vente/recap.php
+++ views/Vente/recap.php
@@ -1,3 +1,4 @@
+<?php if ($view['totalTTC'] != 0): ?>
 <?php if(isset($_SESSION['client'])) : ?>
     <table width="300px" style="float:left;">  
         <tr >
@@ -85,41 +86,49 @@
             <td></td>
         </tr>
     </table>
-<table id="sale_table" width="600px" >
-    <tr><th>Code Barre</th><th>Article</th><th>prix HT</th><th>prix TTC</th><th>TVA</th></tr>
-</table>
-<div style="height:100px;overflow:auto;">
-    <table id="sale_table" width="600px">
-        <?php if(isset($_SESSION['panier'])) : ?>
-            <?php foreach($_SESSION['panier'] as $article) : ?>
-                <tr >
-                    <td align="center">
-                        <?=$article->CodeBarre?>
+
+    <table id="sale_table" width="600px" >
+        <tr><th>Code Barre</th><th>Article</th><th>prix HT</th><th>prix TTC</th><th>TVA</th></tr>
+    </table>
+    <div style="height:100px;overflow:auto;">
+        <table id="sale_table" width="600px">
+            <?php if(isset($_SESSION['panier'])) : ?>
+                <?php foreach($_SESSION['panier'] as $article) : ?>
+                    <tr >
+                        <td align="center">
+                            <?=$article->CodeBarre?>
+                        </td>
+                        <td align="center"><?=$article->Produit->Libelle?></td>
+                        <td align="center" style=" border-top-style:solid;border-top-width:1px;"><?=$article->Produit->Prix?> f cfa</td>
+                        <td align="center" style=" border-top-style:solid;border-top-width:1px;"><?=$article->Produit->Prix * ( 1 + ($article->Produit->Taxe->Taux/100))?> f cfa</td>
+                        <td align="center" style=" border-top-style:solid;border-top-width:1px;"><?=$article->Produit->Taxe->Taux?>%</td>
+                        </tr>
+                <?php endforeach;?>
+            <?php endif; ?>                     
+        </table>
+    </div>
+    <table width="600px">  
+            <tr >
+                    <td align="left">
+                        <b>Total HT</b>
                     </td>
-                    <td align="center"><?=$article->Produit->Libelle?></td>
-                    <td align="center" style=" border-top-style:solid;border-top-width:1px;"><?=$article->Produit->Prix?> f cfa</td>
-                    <td align="center" style=" border-top-style:solid;border-top-width:1px;"><?=$article->Produit->Prix * ( 1 + ($article->Produit->Taxe->Taux/100))?> f cfa</td>
-                    <td align="center" style=" border-top-style:solid;border-top-width:1px;"><?=$article->Produit->Taxe->Taux?>%</td>
-                    </tr>
-            <?php endforeach;?>
-        <?php endif; ?>                     
-    </table>
-</div>
-<table width="600px">  
-        <tr >
-                <td align="left">
-                    <b>Total HT</b>
-                </td>
-                <td align="right">
-                    <b><?=$view['totalHT']?> f cfa</b>
-                </td>
-        </tr>
-        <tr >
-                <td align="left">
-                    <b>Total TTC</b>
-                </td>
-                <td align="right">
-                    <b><?=$view['totalTTC']?> f cfa</b>
-                </td>
-        </tr>
-    </table>
+                    <td align="right">
+                        <b><?=$view['totalHT']?> f cfa</b>
+                    </td>
+            </tr>
+            <tr >
+                    <td align="left">
+                        <b>Total TTC</b>
+                    </td>
+                    <td align="right">
+                        <b><?=$view['totalTTC']?> f cfa</b>
+                    </td>
+            </tr>
+        </table>
+<?php else:?>
+    <div  id="formKO" class="alert alert-icon alert-danger">
+            <div id="KOText" class="text">
+            Votre Panier est vide. Veuillez choisir au moins un produit.
+            </div>
+    </div>
+<?php endif ?>
